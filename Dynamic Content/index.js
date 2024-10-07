226c1b0a5efd885293c81207081582ec5c83481d
Sushant Bandhani <sushantbandhani22@gmail.com>
2024-10-07 22:58:52 +0530

    Updated Views count function for precision

--- Dynamic Content/index.js
+++ Dynamic Content/index.js
@@ -1,23 +1,21 @@
 function convertViews(num) {
     if (num < 1000000) {
-        const views = (num / 1000).toString() + 'K'
-        return views
+        return (num / 1000).toFixed(1) + 'K';
     }
     else if (num < 1000000000) {
-        const views = (num / 1000000).toString() + 'M'
-        return views
+        return (num / 1000000).toFixed(1) + 'M';
     }
     else {
-        const views = (num / 1000000000).toString() + 'B'
-        return views
+        return (num / 1000000000).toFixed(1) + 'B';
     }
 }
+
 
 function builder(title, cName, views, monthsOld, duration, thumbnail) {
     const Tviews = convertViews(views)
     const ele = document.getElementsByClassName("container")[0];
     ele.insertAdjacentHTML('beforeend', `
-        <div class="content">
+        <div class="content" id="redirectDiv">
             <div class="contentImage">
                 <img src="${thumbnail}" />
                 <span>${duration}</span>
@@ -28,15 +26,20 @@ function builder(title, cName, views, monthsOld, duration, thumbnail) {
             </div>
         </div>
     `);
+    document.getElementById("redirectDiv").addEventListener("click", function () {
+        window.location.href = "https://www.youtube.com/watch?v=ApXoWvfEYVU"; // Redirect to Sunflower song
+    });
 }
 
 function submit() {
     const Title = document.getElementById("Title").value;
     const Channel = document.getElementById("Channel").value;
-    const views = document.getElementById("Views").value;
+    const views = document.getElementById("views").value;
     const Months = document.getElementById("Months").value;
     const Time = document.getElementById("Time").value;
     const Image = document.getElementById("Image").value;
     builder(Title, Channel, views, Months, Time, Image)
-    /* builder("JavaScript Exercise 13 - Dynamic Website Builder | Sigma Web Development Course - Tutorial #73","code with harry", 500000000,7,"31:22","https://tse4.mm.bing.net/th/id/OIP.PAUo8-tavpOkHgCMi6M3UAHaD3?w=284&h=180&c=7&r=0&o=5&pid=1.7") */
+    /* builder("Post Malone, Swae Lee - Sunflower (Spider-Man: Into the Spider-Verse) (Official Video)","Post Malone", 2749054240,60,"2:42","https://i.ytimg.com/vi/ApXoWvfEYVU/hqdefault.jpg?sqp=-oaymwEbCKgBEF5IVfKriqkDDggBFQAAiEIYAXABwAEG&rs=AOn4CLD9YG0rLWYh_4OR7Gg5_hk_9mu76w") */
+    /* builder("The Weeknd - Save Your Tears (Official Music Video)","The Weeknd", 1699669530,36,"4:08","images/weekend") */
 }
+
