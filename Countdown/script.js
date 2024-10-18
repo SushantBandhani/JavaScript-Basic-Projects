3ce6d5c1167dc72a1d604e1933c5e28551804d23
Sushant Bandhani <sushantbandhani22@gmail.com>
2024-10-18 06:50:25 +0530

    Adding countdown functionality

---
+++ Countdown/script.js
@@ -0,0 +1,19 @@
+const counters=document.querySelectorAll('.counter')
+
+counters.forEach((counter)=>{
+    counter.innerHTML=0;
+// Using setInterval for repititive updation of innerHTML of our element    
+    let a=setInterval(()=>{
+        const targetCount=Number(counter.getAttribute('data-target'))
+        const startCount=Number(counter.innerHTML)
+        const incr=targetCount/100;
+
+        if(startCount<targetCount){
+            counter.innerHTML=`${Math.round(startCount+incr)}`
+        }
+        else{
+            counter.innerHTML=targetCount
+            clearInterval(a)
+        }
+    },30)
+})
